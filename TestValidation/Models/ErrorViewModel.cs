5c553a760c86f256a81fd45cff6cc0f5a5d2cd2d
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-07-25 10:15:32 +0300

    Deleted Test UI Layer

--- TestValidation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TestValidation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
