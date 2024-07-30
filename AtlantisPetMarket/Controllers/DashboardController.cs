1506799fd3a312d0aad58d4a4b032cf578a93246
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-07-30 15:46:57 +0300

    Added Admin Dashboard

---
+++ AtlantisPetMarket/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtlantisPetMarket.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
