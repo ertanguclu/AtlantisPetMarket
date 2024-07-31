e10046eae8eedd660b78910ad5aed1fe0d7c4feb
kocaksey <seyhunkocak11@gmail.com>
2024-07-31 12:06:49 +0300

    theme updated

--- EntityLayer/Migrations/20240731075551_initialCreate.Designer.cs
+++ EntityLayer/Migrations/20240731075551_initialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EntityLayer.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240731075551_initialCreate")]
+    partial class initialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
