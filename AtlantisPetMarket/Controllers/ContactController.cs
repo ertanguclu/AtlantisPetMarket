c63e035e1fc4dbfdd8346c34522fcf4959df6961
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-07-31 21:27:45 +0300

    Added Contact Entity and Controller

---
+++ AtlantisPetMarket/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtlantisPetMarket.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
