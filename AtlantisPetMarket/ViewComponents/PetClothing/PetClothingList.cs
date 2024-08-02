cd3126832db78ebc0e2e75ef4d7c70824f0e5189
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-08-02 14:41:16 +0300

    Added PetClothingComponent

---
+++ AtlantisPetMarket/ViewComponents/PetClothing/PetClothingList.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.DbContexts;
+using EntityLayer.Models.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtlantisPetMarket.ViewComponents.PetClothing
+{
+
+
+    public class PetClothingList : ViewComponent
+    {
+        private readonly IProductManager<AppDbContext, Product, int> _productManager;
+        public PetClothingList(IProductManager<AppDbContext, Product, int> productManager)
+        {
+            _productManager = productManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var values = await _productManager.GetAllIncludeAsync(x => x.CategoryId == id, x => x.Category);
+            return View(values);
+        }
+    }
+
+}
