bcf286719556ea78b78284d6cb48a6ea0023933f
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-08-03 21:03:28 +0300

    Add Foodies Components

---
+++ AtlantisPetMarket/Controllers/PetFoodiesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtlantisPetMarket.Controllers
+{
+
+    public class PetFoodiesController : Controller
+    {
+
+        public async Task<IActionResult> LoadFoods(string category)
+        {
+            if (category == "Kedi Maması")
+            {
+                return ViewComponent("CatFoodsList");
+            }
+            else if (category == "Köpek Maması")
+            {
+                return ViewComponent("DogFoodsList");
+            }
+            else if (category == "Kuş Yemi")
+            {
+                return ViewComponent("BirdFoodsList");
+            }
+            else if (category == "Balık Yemi")
+            {
+                return ViewComponent("FishFoodsList");
+            }
+            else if (category == "Kemirgen Yemi")
+            {
+                return ViewComponent("RodentFoodsList");
+            }
+            else
+            {
+                return ViewComponent("GetAllFoodsList");
+            }
+        }
+    }
+}
