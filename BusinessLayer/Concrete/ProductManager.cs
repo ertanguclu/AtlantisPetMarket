72b5cd617c7603b409f79115def996345b8544b1
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-08-04 13:30:26 +0300

    Deleted ProductManager Method

--- BusinessLayer/Concrete/ProductManager.cs
+++ BusinessLayer/Concrete/ProductManager.cs
@@ -13,17 +13,6 @@ namespace BusinessLayer.Concrete
         {
         }
 
-        //public async Task<IEnumerable<T>> GetProductsByCategoryAsync(string categoryName)
-        //{
-        //    IQueryable<Product> query = context.Set<Product>().Include(p => p.Category);
-
-        //    if (!string.IsNullOrWhiteSpace(categoryName))
-        //    {
-        //        query = query.Where(p => p.Category.CategoryName == categoryName);
-        //    }
-
-        //    return await query.Cast<T>().ToListAsync();
-        //}
         public async Task<IEnumerable<T>> GetProductsByCategoryAsync(Expression<Func<T, bool>>? filter, params Expression<Func<T, object>>[] include)
         {
             IQueryable<T> query = context.Set<T>();
