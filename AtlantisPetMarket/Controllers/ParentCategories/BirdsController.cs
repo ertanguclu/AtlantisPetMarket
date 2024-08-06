b87f17e15a626ca6501074f33bc621fb0d61e619
1mertyuksel <mertyuksel680@gmail.com>
2024-08-06 22:29:12 +0300

    Updated ParentCategories Controllers and Added related View Page.

--- AtlantisPetMarket/Controllers/ParentCategories/BirdsController.cs
+++ AtlantisPetMarket/Controllers/ParentCategories/BirdsController.cs
@@ -1,4 +1,6 @@
-﻿using BusinessLayer.Abstract;
+﻿using AtlantisPetMarket.Models.ProductVM;
+using AutoMapper;
+using BusinessLayer.Abstract;
 using BusinessLayer.Concrete;
 using EntityLayer.DbContexts;
 using EntityLayer.Models.Concrete;
@@ -11,25 +13,30 @@ namespace AtlantisPetMarket.Controllers.ParentCategories
     {
         private readonly IProductManager<AppDbContext, Product, int> _productManager;
         private readonly IParentCategoryManager<AppDbContext, ParentCategory, int> _parentCategoryManager;
+        private readonly IMapper _mapper;
 
-        public BirdsController(IProductManager<AppDbContext, Product, int> productManager, IParentCategoryManager<AppDbContext, ParentCategory, int> parentCategoryManager)
+        public BirdsController(IProductManager<AppDbContext, Product, int> productManager, IParentCategoryManager<AppDbContext, ParentCategory, int> parentCategoryManager, IMapper mapper)
         {
             _productManager = productManager;
             _parentCategoryManager = parentCategoryManager;
+            _mapper = mapper;
+
+
         }
 
         public async Task<IActionResult> Index()
         {
-            var catCategory = await _parentCategoryManager.GetByAsync(pc => pc.ParentCategoryName == "Kuş");
+            var birdCategory = await _parentCategoryManager.GetByAsync(pc => pc.ParentCategoryName.ToLower() == "kuş");
 
-            if (catCategory == null)
+            if (birdCategory == null)
             {
                 return NotFound();
             }
 
-            var products = await _productManager.GetAllAsync(p => p.ParentCategoryId == catCategory.Id);
+            var products = await _productManager.GetAllAsync(p => p.ParentCategoryId == birdCategory.Id);
+            var birdViewModel = _mapper.Map<IEnumerable<ProductListVM>>(products);
 
-            return View(products);
+            return View(birdViewModel);
         }
     }
 }
--- AtlantisPetMarket/Controllers/ParentCategories/RodentsController.cs
+++ AtlantisPetMarket/Controllers/ParentCategories/RodentsController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AtlantisPetMarket.Models.ProductVM;
+using AutoMapper;
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using EntityLayer.DbContexts;
+using EntityLayer.Models.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AtlantisPetMarket.Controllers.ParentCategories
 {
     public class RodentsController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductManager<AppDbContext, Product, int> _productManager;
+        private readonly IParentCategoryManager<AppDbContext, ParentCategory, int> _parentCategoryManager;
+        private readonly IMapper _mapper;
+
+        public RodentsController(IProductManager<AppDbContext, Product, int> productManager, IParentCategoryManager<AppDbContext, ParentCategory, int> parentCategoryManager, IMapper mapper)
         {
-            return View();
+            _productManager = productManager;
+            _parentCategoryManager = parentCategoryManager;
+            _mapper = mapper;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var rodentCategory = await _parentCategoryManager.GetByAsync(pc => pc.ParentCategoryName.ToLower() == "kemirgen");
+
+            if (rodentCategory == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _productManager.GetAllAsync(p => p.ParentCategoryId == rodentCategory.Id);
+            var rodentViewModel = _mapper.Map<IEnumerable<ProductListVM>>(products);
+
+            return View(rodentViewModel);
         }
     }
 }
