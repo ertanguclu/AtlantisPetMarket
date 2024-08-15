bb46e3394de7e4f805ab57d6b8ccb6d7beeaee9b
1mertyuksel <mertyuksel680@gmail.com>
2024-08-15 14:50:33 +0300

    Move scripts from view to wwwroot for better organization.

---
+++ AtlantisPetMarket/wwwroot/js/category-select.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('#ParentCategoryId').change(function () {
+        var parentCategoryId = $(this).val();
+        if (parentCategoryId) {
+            $.ajax({
+                url: getCategoriesUrl,
+                type: "GET",
+                data: { parentCategoryId: parentCategoryId },
+                success: function (data) {
+                    var categorySelect = $('#CategoryId');
+                    categorySelect.empty();
+                    if (data && data.length > 0) {
+                        $.each(data, function (i, category) {
+                            categorySelect.append('<option value="' + category.id + '">' + category.categoryName + '</option>');
+                        });
+                    } else {
+                        categorySelect.append('<option value="">No categories available</option>');
+                    }
+                }
+            });
+        } else {
+            $('#CategoryId').empty().append('<option value="">Lütfen bir üst kategori seçin</option>');
+        }
+    });
+});
