ae6414fd89a4dcf444b52c3971c60b185694dd72
kocaksey <seyhunkocak11@gmail.com>
2024-08-17 18:43:34 +0300

    ss

---
+++ AtlantisPetMarket/wwwroot/js/file-upload.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    'use strict';
+    $(function () {
+        $('.file-upload-browse').off('click').on('click', function () {
+            var file = $(this).parent().parent().parent().find('.file-upload-default');
+            file.trigger('click');
+        });
+        $('.file-upload-default').off('change').on('change', function () {
+            var fileName = $(this).val().replace(/C:\\fakepath\\/i, '');
+            $(this).parent().find('.form-control.file-upload-info').val(fileName);
+        });
+    });
+})(jQuery);
+
