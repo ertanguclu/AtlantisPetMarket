1e3dfc5607747a1d4cd7c504daa45c990c96ee89
1mertyuksel <mertyuksel680@gmail.com>
2024-08-18 16:26:32 +0300

    Added About Us view.

---
+++ AtlantisPetMarket/Controllers/HakkımızdaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtlantisPetMarket.Controllers
+{
+    public class HakkımızdaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
