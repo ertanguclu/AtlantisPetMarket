446c6e1da84c4c42605afdb0a01d729419789a78
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-08-19 00:12:00 +0300

    Added social media vm and validation rules

---
+++ BusinessLayer/ValidationsRules/SocialMediaValidator/SocialMediaInsertValidator.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Models.SocialMediaVM;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationsRules.SocialMediaValidator
+{
+    public class SocialMediaInsertValidator : AbstractValidator<SocialMediaInsertVM>
+    {
+        public SocialMediaInsertValidator()
+        {
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage("Sosyal medya adı boş geçilemez.")
+                .MaximumLength(15).WithMessage("Ad alanı en fazla 15 karakter olabilir.");
+            RuleFor(s => s.Url)
+                .NotEmpty().WithMessage("Url alanı boş geçilemez.");
+            RuleFor(s => s.Icon)
+                .NotEmpty().WithMessage("Icon alanı boş geçilemez.");
+
+        }
+    }
+}
---
+++ BusinessLayer/ValidationsRules/SocialMediaValidator/SocialMediaUpdateValidator.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Models.SocialMediaVM;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationsRules.SocialMediaValidator
+{
+    public class SocialMediaUpdateValidator : AbstractValidator<SocialMediaUpdateVM>
+    {
+        public SocialMediaUpdateValidator()
+        {
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage("Sosyal medya adı boş geçilemez.")
+                .MaximumLength(15).WithMessage("Ad alanı en fazla 15 karakter olabilir.");
+            RuleFor(s => s.Url)
+                .NotEmpty().WithMessage("Url alanı boş geçilemez.");
+            RuleFor(s => s.Icon)
+                .NotEmpty().WithMessage("Icon alanı boş geçilemez.");
+        }
+    }
+}
